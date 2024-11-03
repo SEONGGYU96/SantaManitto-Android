9681b6f7ae8a05fb7c9da6b333faf327411e3d28
HaeTi <t9h9@naver.com>
2024-11-03 16:22:57 +0900

    [REFACTOR] 방 입장 시 응답 처리 수정

--- app/src/main/java/org/sopt/santamanitto/room/join/network/JoinRoomModel.kt
+++ app/src/main/java/org/sopt/santamanitto/room/join/network/JoinRoomModel.kt
@@ -1,18 +1,5 @@
 package org.sopt.santamanitto.room.join.network
 
-data class JoinRoomModel(
-    val room: JoinRoomInfo,
-    val user: JoinRoomUserInfo
-) {
-    data class JoinRoomInfo(
-        val id: Int,
-        val roomName: String,
-        val expiration: String,
-        val invitationCode: String
-    )
-
-    data class JoinRoomUserInfo(
-        val id: Int,
-        val userName: String
-    )
-}
+data class JoinRoomResponseModel(
+    val roomId: String
+)
